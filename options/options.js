1141270526593184801550034820b27f898d45f5
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 14:34:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-frog-dev-99555",
+  url: "https://green-frog-dev-99555.botics.co",
+  api: "https://green-frog-dev-99555.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
